e4d32734e496635d0e5e091340c40e7eef02d99c
star1024 <yang4308@hotmail.com>
2018-08-22 10:23:00 +0800

    highcharts用python測試

---
+++ python_project/get_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+# a = ["A","B","C"]
+# a= pd.DataFrame(a)
+# print(a)
+# print(a)
+# import requests
+# from BeautifulSoup import BeautifulSoup
+# # import shutil
+# res = requests.get('http://www.gamebase.com.tw/forum/64172/topic/96278769/1')
+# soup = BeautifulSoup(res.text)
+# for img in soup.select('.img'):
+#     print(img)
+dfs = pd.read_html('https://rate.bot.com.tw/xrt?Lang=zh-TW')
+currency = dfs[0]
+currency = currency.ix[:,0:5]
+print(currency)
